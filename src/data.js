a7401fae5024a49e55737066ed96d0202703046e
Marija Nadj <marija.nadj5@gmail.com>
2020-03-03 10:43:23 +0100

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,113 @@
+
+
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Cross Street Disc",
+    img: "img/product-1.png",
+    price: 800,
+    company: "Bulls",
+    info:
+      "Frame	Aluminium; Frame size	48 cm, 54 cm, 58 cm, 61 cm; Wheel size	28 inch; Fork SR Suntour NEX- DS HLO; Headset	CH - 9280THBW, 1 - 1 / 8' semi integrated; Rear derailleur:Shimano Deore RD-M592-SGS Shifter:	Shimano SL M2010; Gears	27 speed derailleur gears; Brakes:Shimano Altus BR-MT200/ Disc Brake Hydraulic; Tires:Supero Ranger Anti Puncture",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "Carrera Kraken",
+    img: "img/product-2.png",
+    price: 400,
+    company: "Carrera",
+    info:
+      "Frame: Light, durable and strong 7005 aluminium tubing; Forks: 120mm Suntour Front suspension & lockout feature helps on uphill climbs; Gears: Shimano Acera 27 speed gears have a wider range and more precise shifting; Brakes: Shimano Hydraulic brakes offer superb, controlled stopping in all conditions; Wheels: 27.5 inch wheel and Continental X-King tyres for the grip you need to perform",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "Cross FXT30 26 inch",
+    img: "img/product-3.png",
+    price: 320,
+    company: "Cannondale",
+    info:
+      "Frame	Trail 26, double-butted 6061 Alloy, SAVE, 1-1/8 inch head tube; Fork:	SR Suntour XCT- 100 - 26, 100mm, coil spring, 1 - 1 / 8 inch ; Front Derailleur	Shimano M190, 31.8 clamp; Rear Derailleur	Shimano Acera; Number of Gears	24; Shifters:	Shimano M310 Rapidfire Plus; Chain set	Shimano M171.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "Schwinn Taff 29 inch",
+    img: "img/product-4.png",
+    price: 300,
+    company: "Schwinn",
+    info:
+      "Aluminum mountain-style frame with hydro-formed tubing provides a durable, responsive ride;Powerful front suspension fork soaks up bumps and thumps out on the trail; 24-speed Shimano EZ- Fire trigger shifters and a Shimano rear derailleur provide a wide gear range and quick shifts; Front and rear disc brakes provide crisp all - condition stopping and precise speed control; Big 29 x 2.25 inch MTB StarFlight knobby mountain tires are designed for tough riding on varying terrain; Light and strong alloy rims add durability without weighing you down; Schwinn alloy crank supplies optimal gearing; Tool - free adjustable seat post for easy height adjustments on the go.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "Tricross Elite Steel Disc",
+    img: "img/product-5.png",
+    price: 400,
+    company: "Elite",
+    info:
+      "Frame	Specialized Premium Steel;Fork: Specialized Tricross steel disc fork; Rims/ Wheels:	Axis 2.0; Hubs:	See Rims / Wheels;Spokes:	See Rims / Wheels;Tires: Specialized Borough CX, 700 x 32c w / Armadillo Elite puncture; protection;Crankset:	Shimano Tiagra;Chainrings	50 / 39 / 30",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "CruX Elite",
+    img: "img/product-6.png",
+    price: 990,
+    company: "Specialized",
+    info:
+      "CHAIN	KMC X11 EL, 11-speed w/ Missing Link™;CRANKSET:	Praxis Zayante M30, 1x direct- mount;SHIFT LEVERS	SRAM Rival 1;CASSETTE	SRAM PG - 1130, 11 - 32t;CHAINRINGS:	Praxis Wave Tech, 40T;REAR DERAILLEUR	SRAM Rival 1, long cage; FORK: S- Works FACT 11r carbon, 12x100mm thru - axle; SADDLE	Body Geometry Phenom Comp, hollow Cr-Mo rails;FRONT BRAKE	SRAM Rival 1, flat-mount disc; REAR BRAKE	SRAM Rival 1, flat- mount disc.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "GT Avalanche Sport",
+    img: "img/product-7.png",
+    price: 650,
+    company: "GT",
+    info:
+      "Bottom Bracket	Cartridge Sealed; Brake: Levers	Tektro disc;Brake Type:	Hydraulic Disc Brake;Cassette:	Shimano Alivio, 11-34, 9-speed;Chain:	KMC Z99, 9-speed;Crankset:	All Terra, 44/32/22;Derailleur Front:	Shimano Altus, 34.9 clamp;Derailleur Rear:	Shimano Alivio;Fork:	SR Suntour XCT - DS, 100mm, hydraulic lockout, Coil; Frame	6061 T6 Aluminum Frame, w/ Triple Triangle™, Replaceable Derailleur Hanger, Disc Brake Mounts, and Zerostack tapered 1 1/8-1 1/2Head Tube, 27.5/29 Design",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "Shimano 700C 52cm",
+    img: "img/product-8.png",
+    price: 800,
+    company: "Shimano",
+    info:
+      "Weight Capacity:440 lbs;Unit Weight:32 lbs;Strong and Lightweight Aluminum Frame;Fashionable Design, Racing Appearance;Quick Release;Enough Power and speed to meet commuting, racing or traveling needs;Shimano 21 speed TZ50 shifter and derailleur;Comfortable and Adjustable Handle Bar and seat post;Recommended height: 5'5-6'0 inch;Kickstand for convenient parking;Specification:Size: Medium(52cm); Derailleur Shifter: Shimano A050 Thumb;Frame & Fork : Aluminum Alloy 700 c;Material of Saddle: PVC fake leather; Handle Stem: Steel;Pedals: Alloy 9 / 16 with ball;Spoke:700C*1.5*14G*36H;Front Derailleur: TZ31(Shimano); Rear Derailleur: TZ50(Shimano);Brake: Alloy Caliper Brake; Kickstand: Included.",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
+
+export const detailProduct = {
+  id: 1,
+    title: "Cross Street Disc",
+      img: "img/product-1.png",
+        price: 800,
+          company: "Bulls",
+            info:
+  "Frame	Aluminium; Frame size	48 cm, 54 cm, 58 cm, 61 cm; Wheel size	28 inch; Fork SR Suntour NEX- DS HLO; Headset	CH - 9280THBW, 1 - 1 / 8' semi integrated; Rear derailleur:Shimano Deore RD-M592-SGS Shifter:	Shimano SL M2010; Gears	27 speed derailleur gears; Brakes:Shimano Altus BR-MT200/ Disc Brake Hydraulic; Tires:Supero Ranger Anti Puncture",
+    inCart: false,
+      count: 0,
+        total: 0
+};
